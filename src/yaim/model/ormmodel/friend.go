2fa1bfd67ba998e0af4717cea325c6612213499a
yulibao <imkrisen@gmail.com>
2019-07-08 01:48:40 +0800

    ip key & firend interfaces

--- src/yaim/model/ormmodel/friend.go
+++
@@ -1,8 +0,0 @@
-package ormmodel
-
-type Friend struct{
-	Id int64
-	Adder string `xorm:"notnull 'adder'"`
-	Receiver string `xorm:"'receiver'"`
-	Validate string `xorm:"default 'no' 'validate'"`
-}
--- src/yaim/model/ormmodel/offline.go
+++
@@ -1,9 +0,0 @@
-package ormmodel
-
-type Offline struct{
-	Id int64
-	Type string `xorm:"notnull 'type'"`
-	Sender string `xorm:"notnull 'sender'"`
-	Receiver string `xorm:"notnull 'receiver'"`
-	Content string `xorm:"varchar(4096)"`
-}
